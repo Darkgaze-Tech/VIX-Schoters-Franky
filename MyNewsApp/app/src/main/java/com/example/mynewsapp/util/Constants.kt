a57f0d81e83da563b9de44597f7f11c405f99cb0
Darkgaze-Tech <64191955+Darkgaze-Tech@users.noreply.github.com>
2022-09-16 13:47:09 +0700

    News App Project

---
+++ MyNewsApp/app/src/main/java/com/example/mynewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.mynewsapp.util
+
+class Constants {
+
+    companion object {
+        const val BASE_URL: String = "https://newsapi.org/"
+        const val API_KEY: String = "f7365d86107943beafc3edcdccaa9522"
+    }
+
+}
